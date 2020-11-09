c5b15662262e0c17c8ad163aced5150220b7f9ab
Seyeon Weon <seyeon.weon@gmail.com>
2020-11-09 17:32:50 +0900

    first publish

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const CompressionPlugin = require('compression-webpack-plugin');
  *****************************************************************************/
 
 // Set base path if your static app does not start from root
-const basePath = ''; // '/spa-github-page-template'
+const basePath = '/spa-github-page-template';
 
 // Set any other dynamic routes in pages
 const dynamicRoutes = {
-  // Pointing `/cute-cats` path to `pages/cats.js`
-  // `pages/cats.js` will also serve `/cats` path 
-  '/cute-cats': { page: '/cats' },
+    // Pointing `/cute-cats` path to `pages/cats.js`
+    // `pages/cats.js` will also serve `/cats` path 
+    '/cute-cats': { page: '/cats' },
 }
 
 /*****************************************************************************
@@ -22,35 +22,33 @@ const dynamicRoutes = {
 const webpackBasePath = process.env.SPA_EXP_BUILD === 'true' ? basePath : ''
 
 module.exports = withSass({
-  publicRuntimeConfig: {
-    basePath: webpackBasePath,
-  },
-  assetPrefix: webpackBasePath,
-  exportPathMap: async function(defaultPathMap) {
-    return Object.assign({}, defaultPathMap, dynamicRoutes);
-  },
-  webpack: function(config) {
-    config.module.rules.push(
-      {
-        test: /\.(jpe?g|png|gif|svg|ico)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-            fallback: 'file-loader',
-            publicPath: `${webpackBasePath}/_next/static/images/`,
-            outputPath: 'static/images/',
-            name: '[name].[hash:15].[ext]',
-          },
-        },
-      }
-    );
+    publicRuntimeConfig: {
+        basePath: webpackBasePath,
+    },
+    assetPrefix: webpackBasePath,
+    exportPathMap: async function(defaultPathMap) {
+        return Object.assign({}, defaultPathMap, dynamicRoutes);
+    },
+    webpack: function(config) {
+        config.module.rules.push({
+            test: /\.(jpe?g|png|gif|svg|ico)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    fallback: 'file-loader',
+                    publicPath: `${webpackBasePath}/_next/static/images/`,
+                    outputPath: 'static/images/',
+                    name: '[name].[hash:15].[ext]',
+                },
+            },
+        });
 
-    config.plugins.push(new CompressionPlugin());
-    config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
-    config.resolve.alias['@helpers'] = path.join(__dirname, 'helpers');
-    config.resolve.alias['@images'] = path.join(__dirname, 'images');
+        config.plugins.push(new CompressionPlugin());
+        config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+        config.resolve.alias['@helpers'] = path.join(__dirname, 'helpers');
+        config.resolve.alias['@images'] = path.join(__dirname, 'images');
 
-    return config;
-  }
+        return config;
+    }
 });
